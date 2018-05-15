a818a06fda3398a5417fdbe8387f92a4f8204ce4
Nicolas Gudino <asternic@gmail.com>
2018-05-15 12:48:40 -0400

    Resize iframe on window resize

--- framework/html/libs/js/iframe.js
+++ framework/html/libs/js/iframe.js
@@ -1,11 +1,26 @@
-function calcHeight()
-{
-  document.getElementById('myframe').height= 580;
-  //find the height of the internal page
-  var altura= document.getElementById('myframe').contentWindow.document.body.scrollHeight;
+function calcHeight() {
 
-  if(altura>=500) {
-    // Cambio la altura 
-    document.getElementById('myframe').height= altura;
-  } 
+    el = document.getElementById('myframe');
+
+    if(el!==null) {
+        el.height= 580;
+        //find the height of the internal page
+        var altura = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        altura = altura - 200;
+        if(altura>=500) {
+            // Cambio la altura 
+            el.height= altura;
+            el.style.height=altura+'px';
+        } 
+    } 
 }
+
+window.onresize = function() { 
+
+   el = document.getElementById('myframe');
+
+   if(el!==null) {
+      calcHeight();
+   }
+}
+

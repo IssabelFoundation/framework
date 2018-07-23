a71c355d3e63e7b42fcf54272b4cc74b894d9ac1
Nicolas Gudino <asternic@gmail.com>
2018-07-23 18:25:37 -0400

    Rebranding

--- framework/html/libs/paloSantoInstaller.class.php
+++ framework/html/libs/paloSantoInstaller.class.php
@@ -3,16 +3,23 @@
   vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4:
   Codificación: UTF-8
   +----------------------------------------------------------------------+
+  | Issabel version 0.5                                                  |
+  | http://www.issabel.org                                               |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2006 Palosanto Solutions S. A.                         |
   | Copyright (c) 1997-2003 Palosanto Solutions S. A.                    |
   +----------------------------------------------------------------------+
-  | Este archivo fuente está sujeto a las políticas de licenciamiento    |
-  | de Palosanto Solutions S. A. y no está disponible públicamente.      |
-  | El acceso a este documento está restringido según lo estipulado      |
-  | en los acuerdos de confidencialidad los cuales son parte de las      |
-  | políticas internas de Palosanto Solutions S. A.                      |
-  | Si Ud. está viendo este archivo y no tiene autorización explícita    |
-  | de hacerlo, comuníquese con nosotros, podría estar infringiendo      |
-  | la ley sin saberlo.                                                  |
+  | The contents of this file are subject to the General Public License  |
+  | (GPL) Version 2 (the "License"); you may not use this file except in |
+  | compliance with the License. You may obtain a copy of the License at |
+  | http://www.opensource.org/licenses/gpl-license.php                   |
+  |                                                                      |
+  | Software distributed under the License is distributed on an "AS IS"  |
+  | basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See  |
+  | the License for the specific language governing rights and           |
+  | limitations under the License.                                       |
+  +----------------------------------------------------------------------+
+  | The Initial Developer of the Original Code is PaloSanto Solutions    |
   +----------------------------------------------------------------------+
   | Autores: Gladys Carrillo B.   <gcarrillo@palosanto.com>              |
   +----------------------------------------------------------------------+

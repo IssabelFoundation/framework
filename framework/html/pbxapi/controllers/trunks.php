2fff128132137a155cfdee1df7c8805c089aa55c
Nicolas Gudino <asternic@gmail.com>
2018-08-28 11:17:55 -0400

    Add basic field maps

--- framework/html/pbxapi/controllers/trunks.php
+++ framework/html/pbxapi/controllers/trunks.php
@@ -7,6 +7,17 @@ class trunks extends rest {
     protected $dest_field = "";
     protected $list_fields = array('tech','channelid');
     protected $extension_field = '';
+
+    protected $field_map = array(
+        'channelid'          => 'trunk_name',
+        'usercontext'        => 'user_context',
+        'maxchans'           => 'maximum_channels',
+        'outcid'             => 'outbound_callerid',
+        'dialoutprefix'      => 'dialout_prefix',
+        'continue'           => 'continue_if_busy',
+ 
+
+    );
 }
 
 

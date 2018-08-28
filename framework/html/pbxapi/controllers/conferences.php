5c467530f779ec19ceb30d74f9c5c7fd1cc47c04
Nicolas Gudino <asternic@gmail.com>
2018-08-27 20:06:53 -0400

    Set extension field

--- framework/html/pbxapi/controllers/conferences.php
+++ framework/html/pbxapi/controllers/conferences.php
@@ -3,6 +3,7 @@
 class conferences extends rest {
     protected $table      = "meetme";
     protected $id_field   = 'exten';
+    protected $extension_field   = 'exten';
     protected $name_field = 'description';
     protected $dest_field = 'CONCAT("from-internal",",",exten,",1")';
     protected $search_field = 'description';

54c3ed505eaa6cb4c960eb208b9a96648b83ae0a
Nicolas Gudino <asternic@gmail.com>
2018-09-04 09:57:55 -0400

    Add GPL headers. Reduce token time validity to two hours

--- framework/html/pbxapi/models/jwtauth.php
+++ framework/html/pbxapi/models/jwtauth.php
@@ -1,4 +1,30 @@
 <?php
+/* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4:
+  Codificación: UTF-8
+  +----------------------------------------------------------------------+
+  | Issabel version 4.0                                                  |
+  | http://www.issabel.org                                               |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2018 Issabel Foundation                                |
+  +----------------------------------------------------------------------+
+  | This program is free software: you can redistribute it and/or modify |
+  | it under the terms of the GNU General Public License as published by |
+  | the Free Software Foundation, either version 3 of the License, or    |
+  | (at your option) any later version.                                  |
+  |                                                                      |
+  | This program is distributed in the hope that it will be useful,      |
+  | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+  | GNU General Public License for more details.                         |
+  |                                                                      |
+  | You should have received a copy of the GNU General Public License    |
+  | along with this program.  If not, see <http://www.gnu.org/licenses/> |
+  +----------------------------------------------------------------------+
+  | The Initial Developer of the Original Code is Issabel LLC            |
+  +----------------------------------------------------------------------+
+  $Id: jwtauth.php, Tue 04 Sep 2018 09:57:13 AM EDT, nicolas@issabel.com
+*/
+
 use Firebase\JWT\JWT;
 
 class jwtauth {
@@ -15,7 +41,6 @@ class jwtauth {
 
         // Use always CORS header, no matter the outcome
         $f3->set('CORS.origin','*');
-        //header("Access-Control-Allow-Origin: *");
 
         try {
             $this->data = new DB\SQL\Mapper($this->db,'acl-user');
@@ -140,7 +165,7 @@ class jwtauth {
 
             $tokenrefresh = array(
                 'iat' => $time,
-                'exp' => $time + ( $exp * 24 ),
+                'exp' => $time + ( $exp * 2 ),
                 'data' => [ ]
             );
 

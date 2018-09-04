3a30b9bf0ac07bb93af6af6c4f8015e5e23ec21b
Nicolas Gudino <asternic@gmail.com>
2018-09-04 09:58:03 -0400

    Add GPL headers

--- framework/html/pbxapi/controllers/extensions.php
+++ framework/html/pbxapi/controllers/extensions.php
@@ -1,4 +1,29 @@
 <?php
+/* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4:
+  Codificación: UTF-8
+  +----------------------------------------------------------------------+
+  | Issabel version 4.0                                                  |
+  | http://www.issabel.org                                               |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2018 Issabel Foundation                                |
+  +----------------------------------------------------------------------+
+  | This program is free software: you can redistribute it and/or modify |
+  | it under the terms of the GNU General Public License as published by |
+  | the Free Software Foundation, either version 3 of the License, or    |
+  | (at your option) any later version.                                  |
+  |                                                                      |
+  | This program is distributed in the hope that it will be useful,      |
+  | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+  | GNU General Public License for more details.                         |
+  |                                                                      |
+  | You should have received a copy of the GNU General Public License    |
+  | along with this program.  If not, see <http://www.gnu.org/licenses/> |
+  +----------------------------------------------------------------------+
+  | The Initial Developer of the Original Code is Issabel LLC            |
+  +----------------------------------------------------------------------+
+  $Id: extensions.php, Tue 04 Sep 2018 09:53:19 AM EDT, nicolas@issabel.com
+*/
 
 class extensions extends rest {
     protected $table           = "users";
